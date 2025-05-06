e2a30ae47075064f7c3e773116e8b8cbb9d5924c
Eduzinho009 <115952759+Eduzinho009@users.noreply.github.com>
2025-05-06 17:05:02 -0300

    Add files via upload

---
+++ src/app/serverSide/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from "next"
+import Image from "next/image"
+
+type IData = {
+    results: {
+        imageUrl: string;
+        name:string;
+        films: string;
+        _id: string;
+    }[];
+}
+
+export const metadata: Metadata = {
+    title: "Lista de Personagens RM",
+    description: "Aplicação teste com consumo de api"
+}
+
+const ServerSide = async () => {
+
+    const res = await fetch("https://api.disneyapi.dev/character");
+    const data: IData = await res.json()
+
+  return (
+    <>
+    <div>Server Side Page try</div>
+    {data?.data.map((data, index) => {
+        return(
+            <div className="mb-20" key={index}>
+                <h2>{data._id}</h2>
+                <Image src={data.imageUrl} alt={data.name} width={200} height={200}/>
+                <h1>{data.name}</h1>
+                <p>{data.films}</p>
+            </div>
+        )
+    })}
+    </>
+  )
+}
+
+export default ServerSide
