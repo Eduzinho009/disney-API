09eb9e39d4fe18898bfd709cf2fad65f9b4888fd
Eduzinho009 <115952759+Eduzinho009@users.noreply.github.com>
2025-05-07 15:27:39 -0300

    Add files via upload

--- src/app/axiosPage/page.tsx
+++ src/app/axiosPage/page.tsx
@@ -29,9 +29,10 @@ useEffect(() => {
     return(
         <>
         <section className="bg-[#333333]">
+            <div className="text-[#333333]">""</div>
             <Suspense fallback={<div>Loading...</div>}>
              {data.map((item, index) => (
-                <div key={index} className="border-3 border-[white] w-60 text-center mt-12 h-110 rounded-xl gap-[10rem]">
+                <div key={index} className="border-3 border-[white] w-60 text-center mt-12 h-110 rounded-xl gap-[10rem] ml-200 mt-9">
                     <Image src={item.imageUrl} alt={item.name} width={200} height={200} className="flex ml-5 mt-3"/>
                     <h2 className="text-[white] text-2xl font-bold mt-2 text-center">{item.name}</h2>
                     <p className="text-[#fbc02d] text-sm font-medium mt-2 text-center">{item.allies}</p>
--- src/app/serverSide/page.tsx
+++ src/app/serverSide/page.tsx
@@ -22,17 +22,22 @@ const ServerSide = async () => {
 
   return (
     <>
-    <div>Server Side Page try</div>
+    <section className="bg-[#333333]">
+    <div className="text-[#333333]">||</div>
     {data?.data.map((data, index) => {
         return(
-            <div className="mb-20" key={index}>
-                <h2>{data._id}</h2>
-                <Image src={data.imageUrl} alt={data.name} width={200} height={200}/>
-                <h1>{data.name}</h1>
-                <p>{data.films}</p>
+            <div className="mb-20 border-3 border-[white] w-60 text-center mt-12 h-110 rounded-xl ml-200" key={index}>
+                <h2 className="text-[white] text-2xl font-bold mt-2 text-center">{data._id}</h2>
+                <Image src={data.imageUrl} alt={data.name} width={200} height={200} className="flex ml-5 mt-3"/>
+                <h1 className="text-[white] text-2xl font-bold mt-2 text-center">{data.name}</h1>
+                <p className="text-[#fbc02d] text-sm font-medium mt-2 text-center">{data.films}</p>
             </div>
+
+            
         )
+        
     })}
+    </section>
     </>
   )
 }
